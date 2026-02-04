66e89950eb241d4ad876f1cd84d2459cb185c36a
BrenoNunes96 <brenocp3@live.com>
2026-02-04 10:00:53 -0300

    :sparkles:construçao  da classe conta

---
+++ Classes.ts
@@ -0,0 +1,19 @@
+export class conta{
+nome : string
+
+constructor(nomes:string){
+   this.nome = nomes
+}
+
+}
+
+
+const novoUsuario = new conta ("breno")
+
+console.log(novoUsuario.nome)
+
+export class name {
+    constructor(parameters) {
+        
+    }
+}
