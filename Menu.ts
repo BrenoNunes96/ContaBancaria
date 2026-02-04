be3b989f13344ffc6db141a8cfe2c2a6279b7d69
BrenoNunes96 <brenocp3@live.com>
2026-02-04 10:00:53 -0300

    :sparkles:construçao  da classe conta

---
+++ Menu.ts
@@ -0,0 +1,80 @@
+
+import leia = require('readline-sync');
+import {colors} from "./src/util/Colors"
+export function main (){
+        console.log(colors.bg.black, colors.fg.yellow,"*****************************************************");
+        console.log("                                                     ");
+        console.log("                BANCO DO BRAZIL COM Z                ");
+        console.log("                                                     ");
+        console.log("*****************************************************");
+        console.log("                                                     ");
+        console.log("            1 - Criar Conta                          ");
+        console.log("            2 - Listar todas as Contas               ");
+        console.log("            3 - Buscar Conta por Numero              ");
+        console.log("            4 - Atualizar Dados da Conta             ");
+        console.log("            5 - Apagar Conta                         ");
+        console.log("            6 - Sacar                                ");
+        console.log("            7 - Depositar                            ");
+        console.log("            8 - Transferir valores entre Contas      ");
+        console.log("            9 - Sair                                 ");
+        console.log("                                                     ");
+        console.log("*****************************************************",colors.reset);
+console.log(colors.bg.black, colors.fg.yellow,'digite uma opçao',colors.reset)
+let opcao : number= leia.questionInt('')
+
+if(opcao === 0){
+ console.log(colors.bg.black, colors.fg.yellow,"\n\nBanco do Brazil com Z - O seu Futuro começa aqui!",colors.reset)
+
+    Sobre()
+    console.log(colors.reset,'')
+    process.exit(0)
+}
+
+        switch(opcao){
+case 1:
+    console.log('\n\nCriar Contaz\n\n')
+    break
+case 2:
+    console.log('\n\nListar todas as Contas\n\n')
+    break
+case 3:
+    console.log("\n\n Buscas Conta por Numero \n\n")
+    break
+case 4:
+    console.log("\n\n tualizar Dados da Conta   \n\n")
+   break
+case 5:
+    console.log("\n\n  Apagar Conta     \n\n")
+
+   break
+   case 6:
+    console.log("\n\n Sacar\n\n")
+    break
+case 7:
+       console.log("\n\n Depositar\n\n")
+    break
+case 8:
+       console.log("\n\n Transferir valores entre Contas \n\n")
+    break
+case 9:
+       console.log("\n\n Sair\n\n")
+      
+    break
+default:
+        console.log("invalido")
+
+        }
+
+
+}
+      main()
+            export function Sobre(){
+
+         console.log(colors.bg.black, colors.fg.yellow,"\n*****************************************************");
+    console.log("Projeto Desenvolvido por: Breno Nunes de Almeida");
+    console.log("Generation Brasil - brenogenerationa@generation.org");
+    console.log("github.com/BrenoNunes96");
+    console.log("*****************************************************"),colors.reset
+
+            }
+      
